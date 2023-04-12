dc7ae7af0e7fb6f730241ea94e4d7c34050870ef
JServer <sandersjack141@gmail.com>
2023-04-12 21:20:36 +0100

    Added style

--- VisWithPy/app.py
+++ VisWithPy/app.py
@@ -11,42 +11,101 @@ data = (
     .sort_values(by="Date")
 )
 
-app = Dash(__name__)
+ex_style = [
+    {
+       "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet", 
+    }
+]
+
+app = Dash(__name__,external_stylesheets=ex_style)
+app.title = "Avocado Analytics: Understand Your Avocados!"
 
 app.layout = html.Div(
     children = [
-        html.H1(children="Avocado Analytics"),
-        html.P(
-            children=(
-                "To Analyze the behavior of avocado prices and the number"
-                " of avocados sold in the US between 2015 and 2018"
-            ),
+        html.Div(
+            children=[
+                html.P(children="🥑", className="header-emoji"),
+                html.H1(
+                    children="Avocado Analytics", 
+                    className="header-title"),
+                html.P(
+                    children=(
+                        "To Analyze the behavior of avocado prices and the number"
+                        " of avocados sold in the US between 2015 and 2018"
+                    ),
+                    className="header-description",
+                ),
+            ],
+            className="header",
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["AveragePrice"],
-                        "type": "lines",
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                            {
+                                "x": data["Date"],
+                                "y": data["AveragePrice"],
+                                "type": "lines",
+                                "hovertemplate": (
+                                    "$%{y:.2f}<extra></extra>"
+                                )
+                            },
+                        ],
+                        "layout": {
+                            "title": {
+                                "text": "Average Price of Avocados",
+                                "x": 0.05,
+                                "xanchor": "left",
+                            },
+                            "xaxis": {"fixedragon": True}, 
+                            "yaxis": {
+                                "tickprefix": "S", 
+                                "fixedrange": True,
+                            },
+                            "colorway": ['#17b89'],
+                        },
                     },
-                ],
-                "layout": {"title": "Average Price of Avocados"},
-            },
+                ),
+                className="card",
+            ),
+            html.Div(
+                    children=dcc.Graph(
+                        id="volume-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                "x": data["Date"],
+                                "y": data["Date"],
+                                "type": "lines",
+                                }
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text" : "Avocados Sold",
+                                    "x": 0.05,
+                                    "xanchor":"left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {"fixedrange": True},
+                                "colorway": ["#E12D39"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                    "x": data["Date"],
-                    "y": data["Date"],
-                    "type": "lines",
-                    }
-                ],
-                "layout": {"title": "Avocados Sold"},
-            },
-        ),
-    ]
+    ]  
 )
 
 if __name__ == "__main__":
