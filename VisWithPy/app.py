315638bd136fb447b100af94357764007a0141d2
JServer <sandersjack141@gmail.com>
2023-04-13 17:55:28 +0100

    Added menu

--- VisWithPy/app.py
+++ VisWithPy/app.py
@@ -6,10 +6,13 @@ from dash import Dash, dcc, html
 
 data = (
     pd.read_csv("avocado.csv")
-    .query("type == 'conventional' and region == 'Albany'")
     .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
     .sort_values(by="Date")
 )
+
+regions = data['region'].sort_values().unique()
+avocado_types = data['type'].sort_values().unique()
+
 
 ex_style = [
     {
@@ -41,6 +44,59 @@ app.layout = html.Div(
                 ),
             ],
             className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {'label': region, 'value': region}
+                                for region in regions
+                            ],
+                            value='Albany',
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                children=[
+                    html.Div(children='Type', className='menu-title'),
+                    dcc.Dropdown(
+                        id='type-filter',
+                        options=[
+                            {
+                                'label': avocado_type.title(),
+                                'value': avocado_type,
+                            }
+                            for avocado_type in avocado_types
+                        ],
+                        value='orgainic',
+                        clarable=False,
+                        searchable=False,
+                        className='dropdown',
+                    ),
+                    ],  
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children='Date Range', className='menu-title'
+                        ),
+                        dcc.DatePickerRange(
+                            id='date-range',
+                            min_date_allowed=data['Date'].min().date(),
+                            max_date_allowed=data['Date'].max().date(),
+                            start_date=data['Date'].min().date(),
+                            end_date=data['Date'].max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className='menu'
         ),
         html.Div(
             children=[
@@ -110,3 +166,5 @@ app.layout = html.Div(
 
 if __name__ == "__main__":
     app.run_server(debug=True)
+    
+#Up to Add Interactivity to Your Dash Apps Using Callbacks
