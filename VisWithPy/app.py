165e042f5552ba8f8f562d31d0ab5cb2bfe5e402
JServer <sandersjack141@gmail.com>
2023-04-13 21:15:32 +0100

    Finished leson

--- VisWithPy/app.py
+++ VisWithPy/app.py
@@ -2,7 +2,7 @@
 # Created: 12/04/23
 
 import pandas as pd
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 
 data = (
     pd.read_csv("avocado.csv")
@@ -63,22 +63,22 @@ app.layout = html.Div(
                     ]
                 ),
                 html.Div(
-                children=[
-                    html.Div(children='Type', className='menu-title'),
-                    dcc.Dropdown(
-                        id='type-filter',
-                        options=[
-                            {
-                                'label': avocado_type.title(),
-                                'value': avocado_type,
-                            }
-                            for avocado_type in avocado_types
-                        ],
-                        value='orgainic',
-                        clarable=False,
-                        searchable=False,
-                        className='dropdown',
-                    ),
+                    children=[
+                        html.Div(children='Type', className='menu-title'),
+                        dcc.Dropdown(
+                            id='type-filter',
+                            options=[
+                                {
+                                    'label': avocado_type.title(),
+                                    'value': avocado_type,
+                                }
+                                for avocado_type in avocado_types
+                            ],
+                            value='orgainic',
+                            clearable=False,
+                            searchable=False,
+                            className='dropdown',
+                        ),
                     ],  
                 ),
                 html.Div(
@@ -104,57 +104,13 @@ app.layout = html.Div(
                     children=dcc.Graph(
                         id="price-chart",
                         config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                            {
-                                "x": data["Date"],
-                                "y": data["AveragePrice"],
-                                "type": "lines",
-                                "hovertemplate": (
-                                    "$%{y:.2f}<extra></extra>"
-                                )
-                            },
-                        ],
-                        "layout": {
-                            "title": {
-                                "text": "Average Price of Avocados",
-                                "x": 0.05,
-                                "xanchor": "left",
-                            },
-                            "xaxis": {"fixedragon": True}, 
-                            "yaxis": {
-                                "tickprefix": "S", 
-                                "fixedrange": True,
-                            },
-                            "colorway": ['#17b89'],
-                        },
-                    },
-                ),
+                    ),
                 className="card",
-            ),
-            html.Div(
+                ),
+                html.Div(
                     children=dcc.Graph(
                         id="volume-chart",
                         config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                "x": data["Date"],
-                                "y": data["Date"],
-                                "type": "lines",
-                                }
-                            ],
-                            "layout": {
-                                "title": {
-                                    "text" : "Avocados Sold",
-                                    "x": 0.05,
-                                    "xanchor":"left",
-                                },
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {"fixedrange": True},
-                                "colorway": ["#E12D39"],
-                            },
-                        },
                     ),
                     className="card",
                 ),
@@ -163,6 +119,61 @@ app.layout = html.Div(
         ),
     ]  
 )
+
+@app.callback(
+    Output('price-chart','figure'),
+    Output("volume-chart", "figure"),
+    Input("region-filter", "value"),
+    Input("type-filter", "value"),
+    Input("date-range", "start_date"),
+    Input("date-range", "end_date"),
+)
+
+def update_charts(region, avocado_type, start_date, end_date):
+
+    filtered_data = data.query(
+        "region == @region and type == @avocado_type"
+        " and Date >= @start_date and Date <= @end_date"
+    )
+
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["AveragePrice"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average Price of Avocados",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Total Volume"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {"text": "Avocados Sold", "x": 0.05, "xanchor": "left"},
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
 
 if __name__ == "__main__":
     app.run_server(debug=True)
