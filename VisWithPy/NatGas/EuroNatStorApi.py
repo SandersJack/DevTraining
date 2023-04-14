bb8b56d2d67784832ca03496cf7868b1415f729c
JServer <sandersjack141@gmail.com>
2023-04-14 21:38:08 +0100

    updated api for more info

--- VisWithPy/NatGas/EuroNatStorApi.py
+++ VisWithPy/NatGas/EuroNatStorApi.py
@@ -6,8 +6,8 @@ import Constants
 csv_t = True
 
 if csv_t:
-    w = csv.writer(open("dataSets/DE_NatStorage.csv", 'w'))
-    w.writerow(["Country","Date","full[%]"])
+    w = csv.writer(open("dataSets/NatStorage_Full.csv", 'w'))
+    w.writerow(["Country","Date","Consumption [Twh]","Gas in storage [TWh]","full [%]","Injection [GWh/d]","Injection capacity [GWh/d]","Withdrawal [GWh/d]","Withdrawal capacity [GWh/d]"])
 
 headers = {"x-key": Constants.AGSI_API_KEY}
 url = 'https://agsi.gie.eu/api'
@@ -15,9 +15,15 @@ url = 'https://agsi.gie.eu/api'
 full = []
 date_ = []
 name = []
+gas = []
+inj = []
+inj_cap = []
+withdraw = []
+withdraw_cap = [] 
+consumption = []
     
-start_date = date(2018, 4, 12)
-end_date = date(2023, 4, 12)
+start_date = date(2011, 4, 12)
+end_date = date(2023, 4, 13)
 delta = timedelta(days=1)
 while start_date <= end_date:
     date_s = start_date.strftime("%Y-%m-%d")
@@ -43,11 +49,16 @@ while start_date <= end_date:
 
     #print(jprint(req.json()['data'][1]))
 
-
     for i in range(len(req.json()['data'][0]['children'])):
         name.append(req.json()['data'][0]['children'][i]['name'])
         full.append(req.json()['data'][0]['children'][i]['full'])
         date_.append(req.json()['data'][0]['children'][i]['gasDayStart'])
+        gas.append(req.json()['data'][0]['children'][i]["gasInStorage"])
+        inj.append(req.json()['data'][0]['children'][i]["injection"])
+        withdraw.append(req.json()['data'][0]['children'][i]["withdrawal"])
+        inj_cap.append(req.json()['data'][0]['children'][i]["injectionCapacity"])
+        withdraw_cap.append(req.json()['data'][0]['children'][i]["withdrawalCapacity"])
+        consumption.append(req.json()['data'][0]['children'][i]["consumption"])
         if csv_t:
-            w.writerow([name[-1],date_[-1],full[-1]])
+            w.writerow([name[-1],date_[-1],consumption[-1],gas[-1],full[-1],inj[-1],inj_cap[-1],withdraw[-1],withdraw_cap[-1]])
     
