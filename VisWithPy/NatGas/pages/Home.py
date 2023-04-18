1a3569612293a4c6a8d089e55a5e2210e072526c
JServer <sandersjack141@gmail.com>
2023-04-18 20:01:40 +0100

    Updated home screne with some world data

--- VisWithPy/NatGas/pages/Home.py
+++ VisWithPy/NatGas/pages/Home.py
@@ -1,10 +1,24 @@
 import dash
 from dash import html, dcc
+import pandas as pd
 
 dash.register_page(__name__, path='/')
 
+cont_data = (
+    pd.read_csv("dataSets/continent-data.csv")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y"))
+    .sort_values(by="Date")
+)
+continents = []
+
+for col in cont_data.columns:
+    if "exports" in col:
+        continents.append(col[24:-8])
+
 layout = html.Div(children=[
-    html.H1(children='Welcome to this Dash-Board'),
+    html.Div(children=[
+        html.H1(children='Welcome to this Dash-Board',className="sub-header-title"),
+    ], className="home-sub-title " ),
 
     html.Div(
         children=[
@@ -21,17 +35,98 @@ layout = html.Div(children=[
                 children='''Currently Natural Gas stoage and prices are in the news due to number 
                 of factors which I will try to visulise in this website. '''
              ),
-            ],
+            ], className="home-context",
         ),
     html.Div(children=[
-           html.H2(children='Natural Gas Storage') 
+           html.H2(children='Natural Gas Storage'),
             ],
             className="card"
-        ),
+            ),
     html.Div(children=[
-           html.H2(children='Natural Gas Production') 
+                html.H2(children='Natural Gas Production', className="home-sub-sub-heading"),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children = dcc.Graph(
+                                id="consumtion-chart", 
+                                config={"displayModeBar":False},
+                                figure={
+                                    "data": [
+                                        {
+                                            "x": cont_data["Date"],
+                                            "y": cont_data["Dry natural gas production,{}, Annual".format(continent)],
+                                            "type": "bar",
+                                            "name": continent,                          
+                                            "hovertemplate": (
+                                                "%{y:.2f}<extra></extra>"
+                                            ),
+                                        }
+                                        for continent in continents
+                                    ],
+                                    "layout": {
+                                        "title": {
+                                            "text": "Yearly Production by Continent",
+                                            "x": 0.05,
+                                            "xanchor": "left",
+                                        },
+                                        "xaxis": {"fixedrange": True},
+                                        "yaxis": {
+                                            "title": "Yearly Production[BCF]",
+                                            "fixedrange": True,
+                                        },
+                                        #"colorway": ["#17b897"],
+                                        "barmode": "stack"
+                                    },
+                                },
+                            ),
+                            className = "home-card",
+                            id = "prod",
+                        ),
+                        html.Div(children=["BHA"],id="prodText"),
+                        html.Div(children=["BHA"],id="resText"),
+                        html.Div(
+                            html.Div(
+                            children = dcc.Graph(
+                                id="cont-storage-chart", 
+                                config={"displayModeBar":False},
+                                figure={
+                                    "data": [
+                                        {
+                                            "x": cont_data["Date"],
+                                            "y": cont_data["Natural gas reserves,{}, Annual".format(continent)],
+                                            "type": "bar",
+                                            "name": continent,                          
+                                            "hovertemplate": (
+                                                "%{y:.2f}<extra></extra>"
+                                            ),
+                                        }
+                                        for continent in continents
+                                    ],
+                                    "layout": {
+                                        "title": {
+                                            "text": "Natural Gas Reserves by Continent",
+                                            "x": 0.05,
+                                            "xanchor": "left",
+                                        },
+                                        "xaxis": {"fixedrange": True},
+                                        "yaxis": {
+                                            "title": "Yearly Production[TCF]",
+                                            "fixedrange": True,
+                                        },
+                                        #"colorway": ["#17b897"],
+                                        "barmode": "stack"
+                                    },
+                                },
+                            ),
+                            className = "home-card",
+                            id="res",
+                            ),
+                        ),
+                        
+                    ],
+                ),
             ],
-            className="card"
-        ),
+            className="home-wrapper"
+            ),
     ]
 )
