637823ec2c1994e04f4b39b9a3b646b088be11e0
JackLaptop <sandersjack141@gmail.com>
2023-05-22 13:15:28 +0200

    updates

--- NA62/RICH/RICHAlgoVisSiPM.py
+++ NA62/RICH/RICHAlgoVisSiPM.py
@@ -16,47 +16,32 @@ SiType = 1 # 0 = 3mm, 1 = 6mm, 2 = 9mm
 SpotRadius = 0.30 * m
 
 if(SiType == 0):
-    ConeInputDiameter = 0.003 * m 
+    SensorWidth = 0.003 * m 
     cen_fac = 18/3
     supercell_fac = 75
     nRow = 37
     channels_1 = 33600
 elif SiType == 1:
-    ConeInputDiameter = 0.006 * m 
+    SensorWidth = 0.006 * m 
     cen_fac = 18/6
     supercell_fac = 37
     nRow = 17
     channels_1 = 10000#8496 
 elif SiType == 2:
-    ConeInputDiameter = 0.009 * m
+    SensorWidth = 0.009 * m
     cen_fac = 18/9
     supercell_fac = 25
     nRow = 12
     channels_1 = 3824
 
-ConeInputRadius = 0.5 * ConeInputDiameter
-HoneyCombDistance = ConeInputRadius * np.sqrt(3) 
+ConeInputRadius = 0.5 * SensorWidth
 
 nPM = 0
 #nRow = 12
 nSuperCellinRow = [None]*nRow
-nTriSuperCellinRow = [None]*nRow
 
 PM_positions = [[0 for i in range(2)] for i in range(channels_1)]#8496)]
-alpha = np.arctan(HoneyCombDistance / (5 * ConeInputRadius))
-x_pos = ConeInputRadius
-y_pos = 2 * (ConeInputRadius + HoneyCombDistance)
 
-x_pos_trasf = x_pos * np.cos(alpha) + y_pos * np.sin(alpha)
-y_pos_trasf = -x_pos * np.sin(alpha) + y_pos * np.cos(alpha)
-Row_distance = np.sqrt(pow(x_pos_trasf, 2) + pow(y_pos_trasf, 2))
-
-x_center_tmp = 30 * ConeInputRadius * cen_fac
-y_center_tmp = -6 * HoneyCombDistance * cen_fac
-
-
-angular_coeff = 3.141592654 / 2 - alpha
-q = (-(3.141592654 / 2 - alpha) * x_center_tmp + y_center_tmp)
 cell_dist = [None]*nRow
 
 x_center_row0_up = 0
@@ -69,7 +54,6 @@ y_Center = 0
 
 for i in range(nRow):
     nSuperCellinRow[i] = 0
-    nTriSuperCellinRow[i] = 0
     cell_dist[i] = 0
 
 plo = 0
@@ -116,12 +100,10 @@ def plot(x,y,n,m,l,k,i):
             #plt.draw()
 
 for k in range(nRow):  #first half
-    Chord = 2 * np.sqrt((SpotRadius - Row_distance * k) * (2 * SpotRadius - (SpotRadius - Row_distance * k)))
     nSuperCellinRow[k] = 0
-    nTriSuperCellinRow[k] = 0
-    x_width = np.sqrt(SpotRadius**2 - (k*3*ConeInputDiameter)**2) 
+    x_width = np.sqrt(SpotRadius**2 - (k*3*SensorWidth)**2) 
 
-    while((nSuperCellinRow[k] + 1) * ConeInputDiameter * 8/3 < 2*x_width+10):
+    while((nSuperCellinRow[k] + 1) * SensorWidth * 8/3 < 2*x_width+10):
         nSuperCellinRow[k] +=1
 
     #
@@ -130,11 +112,11 @@ for k in range(nRow):  #first half
     #print(nSuperCellinRow)
 
     if(k != 0):
-        x_width = np.sqrt(SpotRadius**2 - (k*3*ConeInputDiameter)**2) 
-        cell_dist[k] = int(np.floor((300-x_width) / (3*ConeInputDiameter)))
+        x_width = np.sqrt(SpotRadius**2 - (k*3*SensorWidth)**2) 
+        cell_dist[k] = int(np.floor((300-x_width) / (3*SensorWidth)))
 
     if(k == 0):
-        x_center = 600 - (nSuperCellinRow[k]) * ConeInputDiameter * 8/3 /2
+        x_center = 600 - (nSuperCellinRow[k]) * SensorWidth * 8/3 /2
         print(x_center)
     
     triCell = 0
@@ -145,8 +127,8 @@ for k in range(nRow):  #first half
                 if (l%3 == 0):
                     if (m == 2 and n==1):
                         continue
-                    PM_positions[nPM][0] = ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[k] - triCell*ConeInputDiameter
-                    PM_positions[nPM][1] = ConeInputDiameter*m + ConeInputDiameter*k*3
+                    PM_positions[nPM][0] = SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[k] - triCell*SensorWidth
+                    PM_positions[nPM][1] = SensorWidth*m + SensorWidth*k*3
                     
                     plot(PM_positions[nPM][0],PM_positions[nPM][1],n,m,l,k,nPM)
                     plo +=1
@@ -157,10 +139,10 @@ for k in range(nRow):  #first half
                     
                     if (m == 1 and n==1):
                         continue
-                    PM_positions[nPM][0] = ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[k] - triCell*ConeInputDiameter
-                    PM_positions[nPM][1] = ConeInputDiameter*m + ConeInputDiameter*k*3
+                    PM_positions[nPM][0] = SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[k] - triCell*SensorWidth
+                    PM_positions[nPM][1] = SensorWidth*m + SensorWidth*k*3
                     if (m ==2):
-                        PM_positions[nPM][0] = ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[k] - ConeInputDiameter - triCell*ConeInputDiameter
+                        PM_positions[nPM][0] = SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[k] - SensorWidth - triCell*SensorWidth
                     
                     plot(PM_positions[nPM][0],PM_positions[nPM][1],n,m,l,k,nPM)
                     plo +=1
@@ -170,8 +152,8 @@ for k in range(nRow):  #first half
                 if (l%3 == 2):
                     if (m == 0 and n==-1):
                         continue
-                    PM_positions[nPM][0] = ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[k] - ConeInputDiameter - triCell*ConeInputDiameter
-                    PM_positions[nPM][1] = ConeInputDiameter*m + ConeInputDiameter*k*3
+                    PM_positions[nPM][0] = SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[k] - SensorWidth - triCell*SensorWidth
+                    PM_positions[nPM][1] = SensorWidth*m + SensorWidth*k*3
                     
                     plot(PM_positions[nPM][0],PM_positions[nPM][1],n,m,l,k,nPM)
                     plo +=1
@@ -195,8 +177,8 @@ for j in range(nRow):
                 if (l%3 == 0):
                     if (m == 2 and n==1):
                         continue
-                    PM_positions[nPM][0] = (ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[j] - triCell*ConeInputDiameter)
-                    PM_positions[nPM][1] = -(ConeInputDiameter*m + ConeInputDiameter*j*3)
+                    PM_positions[nPM][0] = (SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[j] - triCell*SensorWidth)
+                    PM_positions[nPM][1] = -(SensorWidth*m + SensorWidth*j*3)
                     
                     plot(PM_positions[nPM][0],PM_positions[nPM][1],n,m,l,k,nPM)
                     plo +=1
@@ -207,10 +189,10 @@ for j in range(nRow):
                     
                     if (m == 1 and n==1):
                         continue
-                    PM_positions[nPM][0] = (ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[j] - triCell*ConeInputDiameter)
-                    PM_positions[nPM][1] = -(ConeInputDiameter*m + ConeInputDiameter*j*3)
+                    PM_positions[nPM][0] = (SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[j] - triCell*SensorWidth)
+                    PM_positions[nPM][1] = -(SensorWidth*m + SensorWidth*j*3)
                     if (m ==2):
-                        PM_positions[nPM][0] = (ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[j] - ConeInputDiameter - triCell*ConeInputDiameter)
+                        PM_positions[nPM][0] = (SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[j] - SensorWidth - triCell*SensorWidth)
                     
                     plot(PM_positions[nPM][0],PM_positions[nPM][1],n,m,l,k,nPM)
                     plo +=1
@@ -220,8 +202,8 @@ for j in range(nRow):
                 if (l%3 == 2):
                     if (m == 0 and n==-1):
                         continue
-                    PM_positions[nPM][0] = (ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[j] - ConeInputDiameter - triCell*ConeInputDiameter)
-                    PM_positions[nPM][1] = -(ConeInputDiameter*m + ConeInputDiameter*j*3)
+                    PM_positions[nPM][0] = (SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[j] - SensorWidth - triCell*SensorWidth)
+                    PM_positions[nPM][1] = -(SensorWidth*m + SensorWidth*j*3)
                     
                     plot(PM_positions[nPM][0],PM_positions[nPM][1],n,m,l,k,nPM)
                     plo +=1
@@ -234,8 +216,8 @@ for j in range(nRow):
 '''
         #PM_positions[nPM][0] = (n + 5 * l - 1 * (j + 1) + 5 * (supercell_fac - nSuperCellinRow[j] - cell_dist[j])) * ConeInputRadius
         #PM_positions[nPM][1] = (m - 1 * l - 3 * (j + 1) - 1 * (supercell_fac - nSuperCellinRow[j] - cell_dist[j])) * HoneyCombDistance
-PM_positions[nPM][0] = (ConeInputDiameter*n + ConeInputDiameter*l*3 + 3*ConeInputDiameter*cell_dist[j])
-PM_positions[nPM][1] = -(ConeInputDiameter*m + ConeInputDiameter*j*3 + ConeInputDiameter) 
+PM_positions[nPM][0] = (SensorWidth*n + SensorWidth*l*3 + 3*SensorWidth*cell_dist[j])
+PM_positions[nPM][1] = -(SensorWidth*m + SensorWidth*j*3 + SensorWidth) 
 plot(PM_positions[nPM][0],PM_positions[nPM][1],n,m,l,k)
 #print(PM_positions[nPM][0],PM_positions[nPM][1])
 plo +=1
@@ -260,10 +242,10 @@ if not animate:
         
         #print(PM_positions[i][0],PM_positions[i][1])
         #print(i)
-        rectangle1 = plt.Rectangle((PM_positions[i][0]-x_center,PM_positions[i][1]-y_center), ConeInputDiameter/2, ConeInputDiameter/2, fc=col,angle=ang)
-        rectangle2 = plt.Rectangle((PM_positions[i][0]-x_center,PM_positions[i][1]-y_center), ConeInputDiameter/2, -ConeInputDiameter/2, fc=col,angle=ang)
-        rectangle3 = plt.Rectangle((PM_positions[i][0]-x_center,PM_positions[i][1]-y_center), -ConeInputDiameter/2, ConeInputDiameter/2, fc=col,angle=ang)
-        rectangle4 = plt.Rectangle((PM_positions[i][0]-x_center,PM_positions[i][1]-y_center), -ConeInputDiameter/2, -ConeInputDiameter/2, fc=col,angle=ang)
+        rectangle1 = plt.Rectangle((PM_positions[i][0]-x_center,PM_positions[i][1]-y_center), SensorWidth/2, SensorWidth/2, fc=col,angle=ang)
+        rectangle2 = plt.Rectangle((PM_positions[i][0]-x_center,PM_positions[i][1]-y_center), SensorWidth/2, -SensorWidth/2, fc=col,angle=ang)
+        rectangle3 = plt.Rectangle((PM_positions[i][0]-x_center,PM_positions[i][1]-y_center), -SensorWidth/2, SensorWidth/2, fc=col,angle=ang)
+        rectangle4 = plt.Rectangle((PM_positions[i][0]-x_center,PM_positions[i][1]-y_center), -SensorWidth/2, -SensorWidth/2, fc=col,angle=ang)
         #plt.plot(x,y)
         plt.gca().add_patch(rectangle1)
         plt.gca().add_patch(rectangle2)
