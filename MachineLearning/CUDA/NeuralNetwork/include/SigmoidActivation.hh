30957bcab189d369abdee646c7915e5892233e0b
JackLaptop <sandersjack141@gmail.com>
2023-06-03 20:47:23 +0200

    Init neurla network tutorial walkthrough. Completed layers

---
+++ MachineLearning/CUDA/NeuralNetwork/include/SigmoidActivation.hh
@@ -0,0 +1,17 @@
+#include "NNLayer.hh"
+
+class SigmoidActivation: public NNLayer {
+    public:
+        SigmoidActivation(std::string name);
+        ~SigmoidActivation();
+
+        Matrix& forward(Matrix& Z);
+        Matrix& backprop(Matrix& sA, float learning_rate = 0.01);
+
+    private:
+        Matrix A;
+
+        Matrix Z;
+        Matrix dZ;
+        
+};
